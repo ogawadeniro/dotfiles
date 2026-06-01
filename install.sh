ca55ece30a807b4ad37631bc7a1c6c0ec899ff00
ogawadeniro <ookawa14106@gmail.com>
2026-06-01 20:13:25 +0900

    修正:リンクがない時だけlnコマンドを実行するように修正

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
-ln -s ~/.dotfiles/ghostty ~/.config/ghostty
-ln -s ~/.dotfiles/nvim ~/.config/nvim
-ln -s ~/.dotfiles/tmux ~/.config/tmux
-ln -s ~/.dotfiles/zsh ~/.config/zsh
+# == シンボリックリンクを作成
+# シンボリックリンク作成用ヘルパー
+create_link() {
+    target=$1
+    link=$2
+    # シンボリックリンクがない場合だけ作成する
+    if [ ! -L "$link" ]; then
+        echo "Create $target link to $link"
+        ln -s "$target" "$link"
+    fi
+}
+
+create_link "$HOME/.dotfiles/ghostty" "$HOME/.config/ghostty"
+create_link "$HOME/.dotfiles/nvim" "$HOME/.config/nvim"
+create_link "$HOME/.dotfiles/tmux" "$HOME/.config/tmux"
+create_link "$HOME/.dotfiles/zsh" "$HOME/.config/zsh"
