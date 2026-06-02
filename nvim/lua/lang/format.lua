4d4425b9d6d3df8d58429358579773a7ae8b1e61
ogawadeniro <ookawa14106@gmail.com>
2026-06-02 17:08:07 +0900

    refactor: コメント調整

--- nvim/lua/lang/format.lua
+++ nvim/lua/lang/format.lua
@@ -4,7 +4,9 @@ vim.pack.add({
 local conform = require("conform")
 
 conform.setup({
-    -- ファイルタイプごとにどのフォーマッタを使うか
+    -- ------------------------------------------------------------------------------
+    -- ファイルタイプごとにどのフォーマッタを使うかを設定(each lang)
+    -- ------------------------------------------------------------------------------
     formatters_by_ft = {
         yaml = { "prettier" },
         json = { "prettier" },
@@ -18,7 +20,9 @@ conform.setup({
         bash = { "shfmt" },
     },
 
-    -- フォーマッタの個別設定
+    -- ------------------------------------------------------------------------------
+    -- フォーマッタの個別設定(each lang)
+    -- ------------------------------------------------------------------------------
     formatters = {
         prettier = {},
         black = {
@@ -32,18 +36,22 @@ conform.setup({
         },
     },
 
+    -- ------------------------------------------------------------------------------
+    -- 他設定
+    -- ------------------------------------------------------------------------------
     -- Set this to change the default values when calling conform.format()
     -- This will also affect the default values for format_on_save/format_after_save
     default_format_opts = {
         lsp_format = "fallback",
     },
+    -- 保存時にフォーマットするかどうか
     format_on_save = {
         lsp_fallback = true,
         async = false,
         timeout_ms = 1000,
     },
-    -- Conform will notify you when a formatter errors
+    -- フォーマッタがエラーを履いた時に通知する
     notify_on_error = true,
-    -- Conform will notify you when no formatters are available for the buffer
+    -- 有効なフォーマッタがない時に通知する
     notify_no_formatters = false,
 })
