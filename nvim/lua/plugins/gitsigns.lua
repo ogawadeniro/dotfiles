a97475b3afaf3ce74c90e60c13737a88efb49bd4
ogawadeniro <ookawa14106@gmail.com>
2026-06-02 11:34:23 +0900

    修正:キーマップのプレフィックスと、キーマップの説明を修正

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -11,34 +11,35 @@ require('gitsigns').setup({
             vim.keymap.set(mode, l, r, opts)
         end
         -- Navigation
-        map('n', '<Leader>h]', function()
+        map('n', '<Leader>g]', function()
             if vim.wo.diff then
-                vim.cmd.normal({ '<Leader>hn', bang = true })
+                vim.cmd.normal({ '<Leader>gn', bang = true })
             else
                 gitsigns.nav_hunk('next')
             end
-        end, { desc = "Go to next hunk" })
+        end, { desc = "次のhunk(変更箇所)へ" })
 
-        map('n', '<Leader>h[', function()
+        map('n', '<Leader>g[', function()
             if vim.wo.diff then
-                vim.cmd.normal({ '<Leader>hp', bang = true })
+                vim.cmd.normal({ '<Leader>gp', bang = true })
             else
                 gitsigns.nav_hunk('prev')
             end
-        end, { desc = "Go to previous hunk" })
+        end, { desc = "前のhunk(変更箇所)へ" })
         -- Actions
-        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Stage buffer" })
-        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Reset stage buffer" })
-        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Preview hunk" })
-        -- map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "Undo stage" })
-        map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end,
-            { desc = "See blame on current line" })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = "See diff" })
+        map('n', '<leader>gS', gitsigns.stage_buffer, { desc = "bufferをステージ" })
+        map('n', '<leader>gR', gitsigns.reset_buffer, { desc = "bufferのステージをリセット" })
+        map('n', '<leader>gp', gitsigns.preview_hunk, { desc = "hunk(変更箇所)をプレビュー" })
+        -- map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = "Undo stage" })
+        map('n', '<leader>gb', function() gitsigns.blame_line { full = true } end,
+            { desc = "現在行のblame(最後の変更情報)を確認" })
+        map('n', '<leader>gw', "<Cmd>Gitsigns toggle_word_diff<CR>", { desc = "変更箇所をハイライト" })
+        map('n', '<leader>gD', gitsigns.diffthis, { desc = "diffを確認" })
     end,
     attach_to_untracked          = true,
-    current_line_blame           = true,         -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts      = {
-        virt_text_pos = 'right_align',           -- 'eol' | 'overlay' | 'right_align'
+        virt_text_pos = 'right_align',   -- 'eol' | 'overlay' | 'right_align'
         delay = 500,
     },
     current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
