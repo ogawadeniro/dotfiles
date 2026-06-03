31fb526334ccf5fb32e19cb77bcfd743afc8892c
ogawadeniro <ookawa14106@gmail.com>
2026-06-03 23:11:35 +0900

    feature: コード診断のあいまい検索キーマップを追加

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
     { src = 'https://github.com/nvim-telescope/telescope.nvim' },
     { src = 'https://github.com/nvim-lua/plenary.nvim' },
-    { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim',  name = "telescope-fzf-native" },
+    { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim', name = "telescope-fzf-native" },
 })
 
 -- アップデート時のビルド設定
@@ -21,6 +21,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope Diagnostics' })
 
 -- 他設定
 local opts = {
