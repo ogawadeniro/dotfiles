d2caa7211995454fab115eec7bcdb656e03a6e25
ogawadeniro <ookawa14106@gmail.com>
2026-06-03 23:10:53 +0900

    feature:BufferLineの背景色を透過

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -14,7 +14,7 @@ require("bufferline").setup({
                 text = function()
                     return vim.fn.getcwd()
                 end,
-                  highlight = "Directory",
+                highlight = "Directory",
                 text_align = "left",
                 separator = false,
             }
@@ -24,3 +24,10 @@ require("bufferline").setup({
 
     }
 })
+
+vim.api.nvim_set_hl(0, "BufferLineFill", { link = "StatusLineNC" })
+-- vim.api.nvim_set_hl(0, "BufferLineBufferSelected", { link = "StatusLineNC" })     --選択中のバッファ
+-- vim.api.nvim_set_hl(0, "BufferLineBufferVisible", { link = "StatusLineNC" })      --他バッファ
+--
+-- vim.api.nvim_set_hl(0, "BufferLineDevIconLuaInactive", { link = "StatusLineNC" }) --他バッファ
+-- vim.api.nvim_set_hl(0, "BufferLineDevIconLuaSelected", { link = "StatusLineNC" }) --他バッファ
