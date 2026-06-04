bec36c137162af9b3d87cb13ed9df08f7836a764
ogawadeniro <ookawa14106@gmail.com>
2026-06-04 18:06:09 +0900

    feature: 補完設定をいい感じに

---
+++ zsh/completions.zsh
@@ -0,0 +1,45 @@
+# ==============================================================================
+# 補完
+# ==============================================================================
+# ------------------------------------------------------------------------------
+# == ビルトイン補完設定
+# ------------------------------------------------------------------------------
+# 補完システムを自動ロード
+autoload -Uz compinit
+
+# キャッシュデータファイルから補完を初期化
+compinit -d "$XDG_CACHE_HOME/zsh/zcompdump"
+
+# 補完で大文字小文字を無視
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+# 近似補完を有効化
+zstyle ':completion:*' completer _complete _approximate
+
+# 隠しファイルも補完候補に含める
+zstyle ':completion:*' file-patterns '%p(D) %p'
+# 補完候補から除外するディレクトリ
+zstyle ':completion:*' ignored-patterns '.git' 'node_modules'
+setopt GLOB_DOTS
+
+# 補完候補をメニュー形式で選択する(fzf-tabがあるので無効)
+# zstyle ':completion:*' menu select
+
+# 補完候補の色変え(fzf-tab側でやる)
+# zstyle ':completion:*' list-colors di=34 ln=36 ex=32
+
+# ezaのls補完を再利用する
+compdef eza=ls
+
+# ------------------------------------------------------------------------------
+# == zsh-completions
+_zplugin_load zsh-users zsh-completions
+# ------------------------------------------------------------------------------
+# 補完ソースを追加
+fpath=("${ZDOTDIR}/plugins/zsh-completions/src" $fpath)
+
+# ------------------------------------------------------------------------------
+# == zsh-autosuggestions
+# 履歴から補完
+_zplugin_load zsh-users zsh-autosuggestions
+# ------------------------------------------------------------------------------
--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,34 +1,76 @@
-# == fzf設定
+# ------------------------------------------------------------------------------
+# あいまい検索
+# ------------------------------------------------------------------------------
+
+# == キーバインド読み込み
+if [[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]]; then
+    source /usr/share/doc/fzf/examples/key-bindings.zsh
+    source /usr/share/doc/fzf/examples/completion.zsh
+fi
 
 # ファイル検索コマンド設定(隠しファイル表示)
-if command -v fd >/dev/null 2>&1; then
-    export FZF_DEFAULT_COMMAND='fd --type f --hidden --strip-cwd-prefix'
-elif command -v fdfind >/dev/null 2>&1; then
-    export FZF_DEFAULT_COMMAND='fdfind --type f --hidden --strip-cwd-prefix'
-fi
-
-# Ctrl-Tでfzfを開く
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_COMMAND="${CMD_FD} --type f --hidden --strip-cwd-prefix --exclude .git"
 
 # UIをコンパクトにする
-if command -v bat >/dev/null 2>&1; then
-    export BAT_CMD='bat'
-elif command -v batcat >/dev/null 2>&1; then
-    export BAT_CMD='batcat'
-fi
 export FZF_DEFAULT_OPTS="
     --height 40% 
     --layout=reverse 
     --border 
-    --preview '${BAT_CMD} --style=numbers --color=always {}'
+    --preview '${CMD_BAT} --style=numbers --color=always {}'
 "
 
-# Ctrl+F
-_fzf_file_no_hidden() {
-    local cmd result
-    cmd="${FZF_DEFAULT_COMMAND/--hidden /}"
-    result=$(eval "${cmd:-find . -type f}" : fzf) && LBUFFER+="$result"
+# == キーバインド
+# Ctrl-T ファイル検索
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Ctrl+Rでコマンド履歴を検索
+function fzf-select-history() {
+    BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
+    CURSOR=$#BUFFER
     zle reset-prompt
 }
+zle -N fzf-select-history
 
-zle -N _fzf_file_no_hidden
+# Ctrl+bで移動履歴を辿れる
+function fzf-cdr() {
+    local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --reverse)
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N fzf-cdr
+
+# ------------------------------------------------------------------------------
+# == fzf-tab設定
+_zplugin_load aloxaf fzf-tab
+# ------------------------------------------------------------------------------
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+# custom fzf flags
+# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept --preview-window=right:80%
+# To make fzf-tab follow FZF_DEFAULT_OPTS.
+# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
+# tmuxのポップアップ機能で補完メニューが開く?
+# zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+# ファイルのプレビューを表示
+zstyle ':fzf-tab:complete:*:*' fzf-preview "if [[ -d \$realpath ]]; then eza -la --color=always \$realpath; else ${CMD_BAT} --style=numbers --color=always \$realpath; fi"
+# コマンドのヘルプを表示
+zstyle ':fzf-tab:complete:*:options' fzf-preview 'tldr ${(M)words[1]:-$word} 2>/dev/null || man ${(M)words[1]:-$word} 2>/dev/null || echo "No help available"'
+zstyle ':fzf-tab:complete:*:arguments' fzf-preview 'tldr ${(M)words[1]:-$word} 2>/dev/null || man ${(M)words[1]:-$word} 2>/dev/null || echo "No help available"'
+
+zstyle ':completion::complete:cd:*' fzf-preview "${CMD_FD} --hidden --max-depth 1 --color=always \$word"
