49e956f92eb96e78a5f67863b640d94c7521390e
ogawadeniro <ookawa14106@gmail.com>
2026-06-04 11:18:56 +0900

    feature: cmdhightを0にしたので、Exコマンドのプロンプト表示をいい感じに変更

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,3 +1,16 @@
 vim.pack.add({
-    { src = "https://github.com/stevearc/dressing.nvim" }
+    { src = "https://github.com/stevearc/dressing.nvim" },      -- input系のウィンドウをいい感じにする
+    { src = "https://github.com/rachartier/tiny-cmdline.nvim" } -- vimのcmdlineをいい感じにする
+})
+require("tiny-cmdline").setup({
+    width = { value = "60%" },
+    position = {
+        y = "75%",
+        x = "50%"
+    },
+    menu_col_offset = 1,
+    title = {
+        enabled = true,
+        pos = "left", -- "left" | "center" | "right"
+    },
 })
