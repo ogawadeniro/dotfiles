90bd65b8bedd58135526a6a85ee8dc6e2349980e
ogawadeniro <ookawa14106@gmail.com>
2026-06-10 11:30:31 +0900

    feature: コマンドラインUI向上プラグイン導入(clim)

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,16 +1,4 @@
 vim.pack.add({
-    { src = "https://github.com/stevearc/dressing.nvim" },      -- input系のウィンドウをいい感じにする
-    { src = "https://github.com/rachartier/tiny-cmdline.nvim" } -- vimのcmdlineをいい感じにする
-})
-require("tiny-cmdline").setup({
-    width = { value = "60%" },
-    position = {
-        y = "75%",
-        x = "50%"
-    },
-    menu_col_offset = 1,
-    title = {
-        enabled = true,
-        pos = "left", -- "left" | "center" | "right"
-    },
+    { src = "https://github.com/stevearc/dressing.nvim" }, -- input系のウィンドウをいい感じにする
+    { src = "https://github.com/ogawadeniro/clim" },       -- cmdlineをいい感じにする
 })
