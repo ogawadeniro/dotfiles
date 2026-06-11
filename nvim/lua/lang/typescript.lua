5c6b575b3fa0289a80b094fb64404ce9e05425d9
ogawadeniro <ookawa14106@gmail.com>
2026-06-11 22:37:33 +0900

    fix: typescriptのインデント幅を2にした

--- nvim/lua/lang/typescript.lua
+++ nvim/lua/lang/typescript.lua
@@ -5,4 +5,22 @@ vim.pack.add({
 })
 
 
-require("typescript-tools").setup {}
+require("typescript-tools").setup({
+    -- その他の設定 (on_attachなど)
+    settings = {
+        -- ...
+        tsserver_format_options = {
+            convertTabsToSpaces = true,
+            indentSize = 2,
+            tabSize = 2,
+        },
+    },
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { "typescript", "typescriptreact" },
+    callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+    end,
+})
