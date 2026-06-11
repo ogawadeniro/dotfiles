c98eddff4303165666354b1765b05a2b03e95015
ogawadeniro <ookawa14106@gmail.com>
2026-06-11 09:14:43 +0900

    feature: rmをラップして、mvでTrashディレクトリに移動するようにした。

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,6 +14,25 @@ if command -v batcat >/dev/null 2>&1; then
     alias bat='batcat'
 fi
 
+# == rmをラップして、削除したファイルをゴミ箱に入れるようにする
+rm_wrap() {
+    local trash_dir=~/.local/share/Trash/files
+    mkdir -p "$trash_dir"
+    for item in "$@"; do
+        if [ -e "$item" ]; then
+            mv "$item" "$trash_dir"
+            if [ $? -eq 0 ]; then
+                echo "Moved to trash: $item"
+            else
+                echo "Failed to move to trash"
+            fi
+        else
+            echo "rm: cannot remove '$item': No such file or directory" >&2
+        fi
+    done
+}
+alias rm=rm_wrap
+
 # 他
 alias vim="nvim"
 alias grep='rg --color=auto'
