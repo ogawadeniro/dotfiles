8ec46807089c058a7bf5b95f6b5a6f9c679df670
ogawadeniro <ookawa14106@gmail.com>
2026-06-11 22:45:17 +0900

    fix: 猫のオテテをかわいくした

--- zsh/cat.sh
+++ zsh/cat.sh
@@ -2,4 +2,4 @@ os=$(cat /etc/os-release | grep PRETTY_NAME | sed -r 's/PRETTY_NAME="(.+)"/\1/')
 echo "             ╭───────────── ── ─"
 echo "     ∧──∧  .*│ HostName: $(cat /etc/hostname)"
 echo "    ﾐ ･-･ﾐ   │ OS: ${os}"
-echo " 〜ﾐ__u uﾐ   ╰──────────────────────── ── ─ ─"
+echo " 〜ﾐ__O oﾐ   ╰──────────────────────── ── ─ ─"
