a249904201194f9b6f2e585273bb5f1f9679f9fe
ogawadeniro <ookawa@14106>
2026-06-14 16:37:58 +0900

    fix: ゴミ箱に捨てるエイリアス名をrmvにしてrmも使えるようにした。ファイル名重複時削除できない問題も修正。

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -20,18 +20,28 @@ rm_wrap() {
     mkdir -p "$trash_dir"
     for item in "$@"; do
         if [ -e "$item" ]; then
-            mv "$item" "$trash_dir"
-            if [ $? -eq 0 ]; then
-                echo "Moved to trash: $item"
-            else
-                echo "Failed to move to trash"
-            fi
+            local suffix=""
+            for i in $(seq 1 100); do
+                local dst="${trash_dir}/$(basename "$item")${suffix}"
+                # 捨てる先のパスに同じ名前のファイルがなかったら捨てて終わる
+                if [ ! -e "${dst}" ]; then
+                    mv "$item" "$dst"
+                    if [ $? -eq 0 ]; then
+                        echo "Moved to trash: $item"
+                        break
+                    else
+                        echo "Failed to trash: $item"
+                    fi
+                fi
+                # 同じ名前のファイルがあったらサフィックスを変える
+                suffix="_${i}"
+            done
         else
             echo "rm: cannot remove '$item': No such file or directory" >&2
         fi
     done
 }
-alias rm=rm_wrap
+alias rmv=rm_wrap
 
 # 他
 alias vim="nvim"
