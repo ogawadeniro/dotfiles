b8fc7c4633273a3ca7c08ab277fec7f50b0df7f7
ogawadeniro <ookawa14106@gmail.com>
2026-06-16 14:39:24 +0900

    fix: ねこ、合体！！

--- zsh/cat.sh
+++
@@ -1,5 +0,0 @@
-os=$(cat /etc/os-release | grep PRETTY_NAME | sed -r 's/PRETTY_NAME="(.+)"/\1/')
-echo "             ╭───────────── ── ─"
-echo "     ∧──∧  .*│ HostName: $(cat /etc/hostname)"
-echo "    ﾐ ･-･ﾐ   │ OS: ${os}"
-echo " 〜ﾐ__O oﾐ   ╰──────────────────────── ── ─ ─"
