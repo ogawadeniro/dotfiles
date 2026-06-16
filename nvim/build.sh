942dac91ea425bdf32271fe0a316776eac9a1553
ogawadeniro <ookawa14106@gmail.com>
2026-06-16 14:17:44 +0900

    feature: ビルド対象にblink.cmpとLuasnipを追加

--- nvim/build.sh
+++ nvim/build.sh
@@ -2,7 +2,8 @@
 echo "どのプラグインをビルドする？"
 echo "1: peek"
 echo "2: telescope-fzf-native"
-# echo "3: プラグイン名"
+echo "3: blink.cmp"
+echo "4: Luasnip"
 read -p "番号で選択: " selected_num
 
 case ${selected_num} in
@@ -14,9 +15,12 @@ case ${selected_num} in
         # telescope-fzf-native
         echo "telescope-fzf-nativeをビルドするよ"
         (cd ~/.local/share/nvim/site/pack/core/opt/telescope-fzf-native && make);;
-    # "3")
-    # # プラグイン名
-    # ビルドコマンド;;
+    "3")
+        # blink.cmp
+        (cd ~/.local/share/nvim/site/pack/core/opt/blink.cmp && cargo build --release);;
+    "4")
+        # LuaSnip
+        (cd ~/.local/share/nvim/site/pack/core/opt/LuaSnip && make install_jsregexp);;
     *)
         echo "終了するよ";;
 esac
