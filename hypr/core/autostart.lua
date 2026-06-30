01fddf0ac409d9043fcc63516f28103ed3c8c0af
ogawadeniro <ookawa14106@gmail.com>
2026-06-30 20:40:37 +0900

    feature: 起動時にhyprlockを起動するように修正(ディスプレイサーバ風)

--- hypr/core/autostart.lua
+++ hypr/core/autostart.lua
@@ -4,8 +4,9 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd("fcitx5 -d")
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("vicinae server")
     hl.exec_cmd("hyprpaper")
+    hl.exec_cmd("fcitx5 -d")
+    hl.exec_cmd("vicinae server")
+    hl.exec_cmd("waybar")
+    hl.exec_cmd("hyprlock --immediate-render")
 end)
