c8460e9182330cae93a93738ec0083a44ac5883d
ogawadeniro <ookawa14106@gmail.com>
2026-07-02 18:32:15 +0900

    fix: nvim_translator新仕様二対応

--- nvim/lua/plugins/translator.lua
+++ nvim/lua/plugins/translator.lua
@@ -1,9 +1,24 @@
 vim.pack.add({
     { src = "https://github.com/ogawadeniro/nvim-translator" }
 })
-require('nvim-translator').setup({
-    keymap = {
-        { src = "en", dst = "ja", key = "<Leader>?", },
-        { src = "ja", dst = "en", key = "<Leader>g?", },
-    }
-})
+
+-- APIキーをスクリプトで出力してもらって、それをプラグインに設定する。
+local env_path = vim.fn.expand("$HOME/work/scripts/deepl_env.sh")
+if vim.fn.executable(env_path) then
+    local out = vim.fn.system(env_path)
+    local api_key = vim.trim(out)
+    vim.print("api_key=" .. api_key)
+    require('nvim-translator').setup({
+        keymap = {
+            { src = "en", dst = "ja", key = "<Leader>?", },
+            { src = "ja", dst = "en", key = "<Leader>g?", },
+        },
+        client = {
+            provider = "deepl",
+            opt = {
+                url = "https://api-free.deepl.com/v2/translate",
+                api_key = api_key,
+            }
+        }
+    })
+end
