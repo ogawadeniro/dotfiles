4c0797f63f16ebe860f0f0b62283bb9752c721dd
ogawadeniro <ookawa14106@gmail.com>
2026-07-13 22:23:07 +0900

    feature: フォーマッタの自動インストール設定追加

--- nvim/lua/lang/format.lua
+++ nvim/lua/lang/format.lua
@@ -1,26 +1,60 @@
 vim.pack.add({
     { src = "https://github.com/stevearc/conform.nvim", },
+    { src = 'https://github.com/williamboman/mason.nvim' },
+    { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
 })
+
+-- ファイルタイプごとにフォーマッタを指定
+local formatters_by_ft = {
+    -- yaml = { "prettier" },
+    -- json = { "prettier" },
+    make = { "checkmake" },
+    markdown = { "markdownlint" },
+    -- markdown = { "prettier" },
+    python = { "black" },
+    c = { "clang-format" },
+    rust = { "rustfmt" },
+    bash = { "shfmt" },
+    zsh = { "shfmt" },
+    sh = { "shfmt" },
+    toml = { "taplo" }
+}
+local exclude_auto_install = { "rustfmt" }
+
+-- インストールするフォーマッタを抽出
+local install_formatters = {}
+for _, formatters in pairs(formatters_by_ft) do
+    for _, formatter in pairs(formatters) do
+        table.insert(install_formatters, formatter)
+    end
+end
+vim.list.unique(install_formatters)
+-- 自動インストール除外対象のフォーマッタを除外
+for i, formatter in ipairs(install_formatters) do
+    for _, exclude in ipairs(exclude_auto_install) do
+        if formatter == exclude then
+            table.remove(install_formatters, i)
+            break
+        end
+    end
+end
+
+-- フォーマッタ自動インストール設定
+require('mason-tool-installer').setup {
+    ensure_installed = install_formatters,
+    auto_update = false,
+    run_on_start = true,
+}
+
+
+-- conform設定
 local conform = require("conform")
 
 conform.setup({
     -- ------------------------------------------------------------------------------
     -- ファイルタイプごとにどのフォーマッタを使うかを設定(each lang)
     -- ------------------------------------------------------------------------------
-    formatters_by_ft = {
-        -- yaml = { "prettier" },
-        -- json = { "prettier" },
-        make = { "checkmake" },
-        markdown = { "markdownlint" },
-        -- markdown = { "prettier" },
-        python = { "black" },
-        c = { "clang-format" },
-        rust = { "rustfmt" },
-        bash = { "shfmt" },
-        zsh = { "shfmt" },
-        sh = { "shfmt" },
-        toml = { "taplo" }
-    },
+    formatters_by_ft = formatters_by_ft,
 
     -- ------------------------------------------------------------------------------
     -- フォーマッタの個別設定(each lang)
