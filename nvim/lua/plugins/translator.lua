dd02072d3acfd748b1c52958a0e1822f387ae17e
ogawadeniro <ookawa14106@gmail.com>
2026-07-13 22:24:06 +0900

    remove: apikeyプリント削除

--- nvim/lua/plugins/translator.lua
+++ nvim/lua/plugins/translator.lua
@@ -7,7 +7,6 @@ local env_path = vim.fn.expand("$HOME/work/scripts/deepl_env.sh")
 if vim.fn.executable(env_path) then
     local out = vim.fn.system(env_path)
     local api_key = vim.trim(out)
-    vim.print("api_key=" .. api_key)
     require('nvim-translator').setup({
         keymap = {
             { src = "en", dst = "ja", key = "<Leader>?", },
