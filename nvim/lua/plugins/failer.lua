456000558c25f7315b5b6d388c4fee986a8c5304
ogawadeniro <ookawa14106@gmail.com>
2026-07-17 22:38:50 +0900

    fix: サイズ変更&Vimリサイズ時にサイズ自動調整するようにした。

--- nvim/lua/plugins/failer.lua
+++ nvim/lua/plugins/failer.lua
@@ -5,6 +5,7 @@ vim.pack.add({
     { src = "https://github.com/nvim-tree/nvim-tree.lua" },
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
 })
+-- 画像表示
 vim.pack.add({ { src = "https://github.com/3rd/image.nvim" } })
 require("image").setup({
     backend = 'kitty',
@@ -20,112 +21,125 @@ require("image").setup({
     max_height_window_percentage = 50,
 })
 
-local nvimtree = require('nvim-tree')
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-local treeapi = require('nvim-tree.api')
-local icons = require("assets.icons")
+local function setup_nvimtree()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    local nvimtree = require('nvim-tree')
+    local treeapi = require('nvim-tree.api')
+    local icons = require("assets.icons")
 
-local gheight = vim.api.nvim_list_uis()[1].height
-local gwidth = vim.api.nvim_list_uis()[1].width
-local width = math.floor(gwidth / 2)
-local height = gheight - 4
+    local gheight = vim.api.nvim_list_uis()[1].height
+    local gwidth = vim.api.nvim_list_uis()[1].width
+    local MIN_WIDTH = 40
+    local MIN_HEIGHT = 15
+    local width = math.max(math.floor(gwidth / 2), MIN_WIDTH)
+    local height = math.max(math.floor(gheight * 3 / 4), MIN_HEIGHT)
+    local row = gheight - height
+    local col = gwidth - width
 
----@type nvim_tree.config
-local opts = {
-    view = {
-        -- float を有効化する
-        float = {
-            enable = true,
-            quit_on_focus_loss = true, -- ツリー外を非表示（フォーカスが外れたら閉じる）
-            open_win_config = {
-                title = " Nvim Tree ",
-                title_pos = "center",
-                relative = "editor",
-                width = width,
-                height = height,
-                row = math.floor((gheight - height) * 0.5),
-                col = math.floor((gwidth - width) * 0.5),
+    ---@type nvim_tree.config
+    local opts = {
+        view = {
+            -- float を有効化する
+            float = {
+                enable = true,
+                quit_on_focus_loss = true, -- ツリー外を非表示（フォーカスが外れたら閉じる）
+                open_win_config = {
+                    title = " Nvim Tree ",
+                    title_pos = "center",
+                    relative = "editor",
+                    width = width,
+                    height = height,
+                    row = row,
+                    col = col,
+                },
+
             },
-
         },
-    },
-    renderer = {
-        group_empty = false,
-        indent_markers = {
-            enable = true,
-        },
-        icons = {
-            glyphs = {
-                git = {
-                    unstaged = icons.git.unstaged.icon,
-                    staged = icons.git.staged.icon,
-                    -- unmerged = icons.git.unmerged,
-                    -- renamed = icons.git.renamed,
-                    untracked = icons.git.untracked.icon,
-                    deleted = icons.git.deleted.icon,
-                    ignored = icons.git.ignored.icon,
+        renderer = {
+            group_empty = false,
+            indent_markers = {
+                enable = true,
+            },
+            icons = {
+                glyphs = {
+                    git = {
+                        unstaged = icons.git.unstaged.icon,
+                        staged = icons.git.staged.icon,
+                        -- unmerged = icons.git.unmerged,
+                        -- renamed = icons.git.renamed,
+                        untracked = icons.git.untracked.icon,
+                        deleted = icons.git.deleted.icon,
+                        ignored = icons.git.ignored.icon,
+                    }
                 }
             }
-        }
-    },
-    actions = {
-        open_file = {
-            window_picker = {
-                enable = false,
+        },
+        actions = {
+            open_file = {
+                window_picker = {
+                    enable = false,
+                }
+            },
+            file_popup = {
+                open_win_config = {
+                    border = "rounded",
+                },
             }
         },
-        file_popup = {
-            open_win_config = {
-                border = "rounded",
-            },
-        }
-    },
-    filters = {
-        -- dotfiles = true,
-        custom = {},
-    },
-    git = {
-        ignore = false,
-    },
-    on_attach = function(bufnr)
-        local function opts(desc)
-            return {
-                desc = "nvim-tree: " .. desc,
-                buffer = bufnr,
-                noremap = true,
-                silent = true,
-                nowait = true
-            }
-        end
-
-        -- デフォルトマッピング
-        treeapi.map.on_attach.default(bufnr)
-
-        -- カスタムマッピング
-        -- 十字キーでディレクトリ展開（ウィンドウリサイズモード中はリサイズ優先）
-        vim.keymap.set('n', '<Right>', function()
-            if vim.g._resize_mode then
-                vim.cmd("vertical resize +1")
-            else
-                treeapi.node.open.edit()
+        filters = {
+            -- dotfiles = true,
+            custom = {},
+        },
+        git = {
+            ignore = false,
+        },
+        on_attach = function(bufnr)
+            local function opts(desc)
+                return {
+                    desc = "nvim-tree: " .. desc,
+                    buffer = bufnr,
+                    noremap = true,
+                    silent = true,
+                    nowait = true
+                }
             end
-        end, opts('開く'))
-        vim.keymap.set('n', '<Left>', function()
-            if vim.g._resize_mode then
-                vim.cmd("vertical resize -1")
-            else
-                treeapi.node.navigate.parent_close()
-            end
-        end, opts('フォルダを畳む'))
 
-        vim.keymap.set('n', 'zM', treeapi.tree.collapse_all, opts('フォルダをすべて畳む'))
-        vim.keymap.set('n', 'zR', treeapi.tree.expand_all, opts('フォルダをすべて展開'))
-        -- ヘルプの表示/非表示切り替え
-        vim.keymap.set('n', '?', treeapi.tree.toggle_help, opts('Help'))
-    end,
-}
-nvimtree.setup(opts)
+            -- デフォルトマッピング
+            treeapi.map.on_attach.default(bufnr)
+
+            -- カスタムマッピング
+            -- 十字キーでディレクトリ展開（ウィンドウリサイズモード中はリサイズ優先）
+            vim.keymap.set('n', '<Right>', function()
+                if vim.g._resize_mode then
+                    vim.cmd("vertical resize +1")
+                else
+                    treeapi.node.open.edit()
+                end
+            end, opts('開く'))
+            vim.keymap.set('n', '<Left>', function()
+                if vim.g._resize_mode then
+                    vim.cmd("vertical resize -1")
+                else
+                    treeapi.node.navigate.parent_close()
+                end
+            end, opts('フォルダを畳む'))
+
+            vim.keymap.set('n', 'zM', treeapi.tree.collapse_all, opts('フォルダをすべて畳む'))
+            vim.keymap.set('n', 'zR', treeapi.tree.expand_all, opts('フォルダをすべて展開'))
+            -- ヘルプの表示/非表示切り替え
+            vim.keymap.set('n', '?', treeapi.tree.toggle_help, opts('Help'))
+        end,
+    }
+    nvimtree.setup(opts)
+end
+setup_nvimtree()
+
+vim.api.nvim_create_autocmd('VimResized', {
+    callback = function()
+        setup_nvimtree()
+    end
+})
 
 -- highlihght settings
 vim.cmd("hi NvimTreeGitDirty guifg=#f39c12")
